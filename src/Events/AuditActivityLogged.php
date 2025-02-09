6b37f21b171c3c5580acd1fd67c32d497db70933
Stephen Okello Omoding <okellostephenomoding@gmail.com>
2025-02-09 13:22:24 +0300

    v1.5.4

--- src/Events/AuditActivityLogged.php
+++ src/Events/AuditActivityLogged.php
@@ -10,14 +10,20 @@ class AuditActivityLogged
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    protected $user, $action, $message, $request, $before, $after;
+    public $user, $action, $message, $request, $before, $after;
 
-    public function __construct($user, string $action, ?string $message, array $request = [], $before = null, $after = null) {
+   public function __construct($user, string $action, ?string $message, array $request = [], $before = null, $after = null) {
+        $this->user = $user;
+        $this->action = $action;
+        $this->message = $message;
         $this->request = [
             'method'     => $request['method'] ?? null,
             'url'        => $request['url'] ?? null,
             'ip'         => $request['ip'] ?? null,
             'user_agent' => $request['user_agent'] ?? null,
         ];
+        $this->before = $before;
+        $this->after = $after;
     }
+
 }
